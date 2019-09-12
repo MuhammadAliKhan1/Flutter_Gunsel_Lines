3a5a354603cef01fa37733dc2323eec78e9ec0ff
AbdulMustafa97 <Abdul.Mustafa.amd@gmail.com>
2019-09-12 15:47:13 +0500

    updated drawer icons

--- lib/data/constants.dart
+++ lib/data/constants.dart
@@ -42,7 +42,10 @@ const AssetImage swappingIcon = AssetImage("assets/swapping_icon.png");
 const AssetImage calendarIcon = AssetImage("assets/calender.png");
 const AssetImage aboutCompanyIcon = AssetImage('assets/aboutcompany.png');
 const AssetImage news = AssetImage("images/news1.webp");
-const AssetImage frontnews = AssetImage("images/front.webp");
+const AssetImage frontNews = AssetImage("images/front.webp");
+const AssetImage newsIcon = AssetImage("assets/newIcon.png");
+const AssetImage buyIcon = AssetImage("assets/buyIcon.png");
+const AssetImage cancelIcon = AssetImage("assets/cencelIcon.png");
 const countryCode = {
   '+48': polandFlag,
   '+42(0': czechFlag,
