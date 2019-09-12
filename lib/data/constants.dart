e3e380a4daec2dac191ed7a54ade0fe060ce9b6d
MuhammadAliKhan1 <khanali198361@gmail.com>
2019-09-12 15:42:21 +0500

    Fixed bugs

--- lib/data/constants.dart
+++ lib/data/constants.dart
@@ -42,7 +42,7 @@ const AssetImage swappingIcon = AssetImage("assets/swapping_icon.png");
 const AssetImage calendarIcon = AssetImage("assets/calender.png");
 const AssetImage aboutCompanyIcon = AssetImage('assets/aboutcompany.png');
 const AssetImage news = AssetImage("images/news1.webp");
-const AssetImage frontnews = AssetImage("images/front.webp");
+const AssetImage frontNews = AssetImage("images/front.webp");
 const countryCode = {
   '+48': polandFlag,
   '+42(0': czechFlag,
