ee9743bd7673c74e5d048cb401d39f9a1907d8c1
MuhammadAliKhan1 <khanali198361@gmail.com>
2019-09-15 22:39:57 +0500

    Profile responsive

--- lib/screens/profile.dart
+++ lib/screens/profile.dart
@@ -30,6 +30,157 @@ class _ProfileScreenState extends State<ProfileScreen> {
   @override
   Widget build(BuildContext context) {
     return Center(
+        child: FittedBox(
+      child: Container(
+        width: MediaQuery.of(context).size.width / 1.2,
+        height: MediaQuery.of(context).size.height / 1.3,
+        child: Stack(
+          children: <Widget>[
+            Container(
+              alignment: Alignment.bottomCenter,
+              width: MediaQuery.of(context).size.width / 1.2,
+              height: MediaQuery.of(context).size.height / 1.2,
+              decoration: BoxDecoration(
+                  borderRadius: BorderRadius.all(
+                Radius.circular(10),
+              )),
+              child: Image(
+                image: person,
+                width: double.infinity,
+                height: MediaQuery.of(context).size.height / 1.3,
+              ),
+            ),
+            Align(
+              alignment: Alignment.topCenter,
+              child: Container(
+                height: 250,
+                width: MediaQuery.of(context).size.width / 1.2 - 46,
+                child: Column(
+                  children: <Widget>[
+                    Container(
+                      padding: EdgeInsets.all(15),
+                      height: 95,
+                      child: Align(
+                        alignment: Alignment.bottomRight,
+                        child: InkWell(
+                          onTap: () {
+                            showDialog(
+                                context: context,
+                                builder: (context) {
+                                  return AlertDialog();
+                                });
+                          },
+                          child: Image(
+                            image: editProfileIcon,
+                            height: 25,
+                          ),
+                        ),
+                      ),
+                    ),
+                    Center(
+                        child: Text(
+                      'Erhan Ozturk',
+                      style: TextStyle(
+                          fontSize: 25,
+                          fontWeight: FontWeight.bold,
+                          fontFamily: "Helvetica"),
+                    )),
+                    SizedBox(
+                      height: 3,
+                    ),
+                    Center(
+                      child: Text(
+                        'eozturk782@gmail.com',
+                        style: TextStyle(
+                          fontSize: 15,
+                          fontFamily: "Helvetica",
+                        ),
+                      ),
+                    ),
+                    SizedBox(
+                      height: 3,
+                    ),
+                    Center(
+                      child: Text(
+                        '+380677331606',
+                        style: TextStyle(
+                          fontSize: 15,
+                          fontFamily: "Helvetica",
+                        ),
+                      ),
+                    ),
+                    SizedBox(
+                      height: 20,
+                    ),
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      mainAxisAlignment: MainAxisAlignment.end,
+                      children: <Widget>[
+                        Row(
+                          crossAxisAlignment: CrossAxisAlignment.center,
+                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                          children: <Widget>[
+                            Column(
+                              children: <Widget>[
+                                Image(
+                                  image: profileScreenLanguageIcon,
+                                  height: 35,
+                                ),
+                                Text('RU',
+                                    style: TextStyle(fontFamily: "Helvetica")),
+                              ],
+                            ),
+                            Column(
+                              children: <Widget>[
+                                Image(
+                                  image: profileScreenLanguageIcon,
+                                  height: 35,
+                                ),
+                                Text('UA',
+                                    style: TextStyle(fontFamily: "Helvetica")),
+                              ],
+                            ),
+                            Column(
+                              children: <Widget>[
+                                Image(
+                                  image: profileScreenLanguageIcon,
+                                  height: 35,
+                                ),
+                                Text('EN',
+                                    style: TextStyle(fontFamily: "Helvetica")),
+                              ],
+                            ),
+                          ],
+                        )
+                      ],
+                    )
+                  ],
+                ),
+              ),
+            ),
+            Align(
+                alignment: Alignment.topCenter,
+                child: Image(
+                  image: profileHolder,
+                  height: 80,
+                )),
+          ],
+        ),
+      ),
+    ));
+  }
+}
+
+/*
+class ProfileScreen extends StatefulWidget {
+  @override
+  _ProfileScreenState createState() => _ProfileScreenState();
+}
+
+class _ProfileScreenState extends State<ProfileScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
         child: Container(
       width: ScreenUtil().setWidth(700),
       height: 550,
@@ -154,3 +305,4 @@ class _ProfileScreenState extends State<ProfileScreen> {
     ));
   }
 }
+ */
