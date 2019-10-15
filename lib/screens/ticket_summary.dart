dbb46c2ddc6a580b9baa65800c9fab4bbda49221
AbdulMustafa97 <Abdul.Mustafa.amd@gmail.com>
2019-10-15 22:26:00 +0500

    image updated in ticket summary

--- lib/screens/ticket_summary.dart
+++ lib/screens/ticket_summary.dart
@@ -661,6 +661,7 @@ class _TicketSummaryTicketState extends State<TicketSummaryTicket> {
     return FittedBox(
       child: Container(
         height: 490,
+        width: 700,
         margin: EdgeInsets.only(
           top: 10.0,
           bottom: 20.0,
@@ -668,7 +669,9 @@ class _TicketSummaryTicketState extends State<TicketSummaryTicket> {
         child: Stack(
           children: <Widget>[
             Image(
-              image: purchasedTicket,
+              image: purchasedTicket2,
+              width: ScreenUtil().setWidth(1300),
+              height: ScreenUtil().setHeight(700),
             ),
             Column(
               children: <Widget>[
